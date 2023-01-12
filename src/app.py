395db2f9f55c1036ba824ce6842da9b350f671a2
Jose L. Gonzalez Diaz <kbcerverjo@gmail.com>
2023-01-12 19:18:34 +0000

    First Commit

---
+++ src/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+from flask import request
+import json
+
+app = Flask(__name__)
+
+todos = [
+    {"label": "My first task", "done": False},
+    {"label": "My second task", "done": False}
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json(force=True)
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    del todos[position]
+    return jsonify(todos)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
